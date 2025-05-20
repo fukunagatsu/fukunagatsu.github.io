d2b06a20f715c36925f727a850ce5c56dbc4f3d8
Tsukasa Fukunaga <fukunaga@aoni.waseda.jp>
2025-05-20 08:58:22 +0000

    250520

--- common.js
+++ common.js
@@ -7,5 +7,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // ナビを読み込む
     fetch("nav.html")
         .then(response => response.text())
-        .then(data => document.getElementById("nav-container").innerHTML = data);
+        .then(data => {
+            document.getElementById("nav-container").innerHTML = data;
+            
+            // ハンバーガーメニュー機能の追加
+            const toggle = document.getElementById("menu-toggle");
+            const navList = document.getElementById("nav-list");
+            
+            if (toggle && navList) {
+                toggle.addEventListener("click", () => {
+                    navList.classList.toggle("show");
+                });
+            }
+        });
 });
